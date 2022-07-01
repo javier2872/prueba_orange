a7db88c5e315879ca08ab00dea801670c19e1aee
javier2872 <43580944+javier2872@users.noreply.github.com>
2022-07-01 17:18:49 +0200

    proyecto updload

---
+++ cypress.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "cypress";
+import * as browserify from "@cypress/browserify-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { preprocessor } from "@badeball/cypress-cucumber-preprocessor/browserify";
+
+export async function setupNodeEvents(
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions
+  ): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    preprocessor(config, {
+      ...browserify.defaultOptions,
+      typescript: require.resolve("typescript"),
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+
+
+export default defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    mochaFile: 'results/prueba_orange_output.xml',
+    toConsole: true
+  },
+  e2e: {
+    specPattern: "**/*.feature",
+    supportFile: false,
+    setupNodeEvents
+  },
+});
